25b43505b2406bd64c15c43ecd7163cd27792a3b
Smiri Mohammad Aziz <smiri.aziz@bld.ai>
2022-11-15 18:42:41 +0100

    task5

--- music_platform/artists/views.py
+++ music_platform/artists/views.py
@@ -7,42 +7,28 @@ from .models import Artist
 from django.views.generic import TemplateView
 from albums.models import Album
 from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from artists.models import Artist
+from .serializers import ArtistSerializer
+from rest_framework.views import APIView
 
 
-class CreateArtist(LoginRequiredMixin, TemplateView):
+class CreateArtist(LoginRequiredMixin, APIView):
 
     login_url = '/signin/'
     redirect_field_name = 'artists'
-    initial = {'key': 'value'}
-    form = ArtistForm
-    template_name = 'artists.html'
 
-    def get(self, request, *args, **kwargs):
-        form = self.form(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form(request.POST)
-        if form.is_valid():
-
-            stage_name = form.cleaned_data['stage_name']
-            social_link = form.cleaned_data['social_link']
-            if (Artist.objects.filter(Stage_name=stage_name).count() > 0):
-                return render(request, 'errors.html')
-
-            else:
-                a = Artist(Stage_name=stage_name, social_link=social_link)
-                a.save()
-
-            return HttpResponseRedirect('/admin/')
+    def post(self, request, format=None):
+        serializer = ArtistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response()
 
 
-class GenerateArtists(TemplateView):
+class GenerateArtists(APIView):
 
-    initial = {'key': 'value'}
-    template_name = 'allartists.html'
-
-    def get(self, request, *args, **kwargs):
-        artists = Artist.objects.all()
-        albums = Album.objects.all()
-        return render(request, self.template_name, {'albums': albums, 'artists': artists})
+    def get(self, request, format=None):
+        aritsts = Artist.objects.all()
+        serializer = ArtistSerializer(aritsts, many=True)
+        return Response(serializer.data)
